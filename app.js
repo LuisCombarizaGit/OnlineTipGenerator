9c16d01e2a1c3ec9c8343753b2fdc62b32dcb740
Luis Combariza <luis_combariza@outlook.com>
2020-11-26 16:36:59 -0800

    Finalized small project, everything working simply run the index.html file in any web-browser

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-
-
-document.getElementById("calculate").addEventListener("click",function(){
-    calculateTip();
-})
+/* Small JavaScipt code that takes care of the functionality of the 
+    web application, by taking the user input and converting it to 
+    the right amount of tip and outputing it back to user.
+*/
 
 function calculateTip(){
     let billAmt = document.querySelector(".billAmt").value;
-    let service = document.querySelector(".serviceSelector").value;
+    let service = document.querySelector("#serviceSelector").value;
     let numofPeople = document.querySelector(".peopleAmt").value;
     
 
-    if(billAmt==="" || service=0){
+    if(billAmt === "" || service == 0){
         alert("Please enter some values");
     }
     
@@ -21,5 +20,22 @@ function calculateTip(){
     else{
         document.getElementById("each").style.display = "block";
     }
+
+    let total = (billAmt * service)/ numofPeople;
+    total = Math.round(total * 100)/ 100;
+    total = total.toFixed(2);
+
+    document.getElementById("totalTip").style.display = "block";
+    document.getElementById("tip").innerHTML= total;
+
+
 }
+
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+
+document.getElementById("calculate").addEventListener("click",function(){
+    calculateTip();
+})
 
